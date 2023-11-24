daecca393508e662f012370e813b364b3fa0087a
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2023-11-24 18:34:09 -0300

    inicio
    Início do projeto

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
 export default function Home(){
-    return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
-     );
+    return (
+        <h1>sssss</h1>
+    )
 }
+// export default function Home(){
+//     const listaLocalStorage = JSON.parse(localStorage.getItem("Listas")) || [];
+//     const [video, setVideo] = useState("")
+//     const [descricao, setDescricao] = useState("")
+//     const [listas, setListas] = useState(listaLocalStorage)
+//     const [identidade, setIdentidade] = useState(listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1)
+
+//     useEffect(() => { localStorage.setItem("Listas", JSON.stringify(listas)) }, [listas])
+
+//     const salvar = (salve) => {
+//         salve.preventDefault()
+//         setListas([...listas, {
+//             video: video,
+//             descricao: descricao,
+//         }])
+//         setIdentidade(identidade + 1)
+//         setNome("")
+
+//         console.log(listas)
+
+//     }
+// }
